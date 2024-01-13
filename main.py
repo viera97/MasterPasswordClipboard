fb4411c32db5ae11ea262c423eafa30678cea14b
viera97 <d.viera1997@gmail.com>
2024-01-12 21:32:03 -0500

    Fixed path problem

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 import pyperclip as pc
 from dotenv import load_dotenv
 
-load_dotenv('pass.env')
+script_dir = os.path.dirname(__file__)
+file_path = os.path.join(script_dir, "pass.env")
+
+load_dotenv(file_path)
 
 Master_Password = os.getenv("Master_Password")
 
